76790aa5254a5d43fe45925b2a22e074c34e7da8
MC <mihail9512@yahoo.co.uk>
2023-09-27 15:45:15 +0200

    Months

--- StenogramExtractor.py
+++ StenogramExtractor.py
@@ -7,7 +7,8 @@ import sys
 import os
 import re
 
-month_dict = { "ноември": 11, "декември": 12}  
+month_dict = { "януари": 1, "февруари": 2,  "март": 3, "април": 4, "май": 5, "юни": 6,
+               "юли": 7, "август": 8, "септември": 9, "октомври": 10, "ноември": 11, "декември": 12}  
 
 def StenogramExtractorMain(yearToQuery):
     driver = webdriver.Edge()
